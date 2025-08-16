566d9c864ac5374d0c9968f4f45d83860c9ae07a
Samuel Rivera <seriveramosq@gmail.com>
2025-08-16 18:34:22 -0500

    perf: background loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
+import { lazy, useState } from "react";
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import { Preloader } from "./components/Preloader/Preloader";
 import { Navbar } from "./components/Navbar/Navbar";
-// import Background from "./components/Background/Background";
 import Hero from "./sections/hero/Hero";
 import Projects from "./sections/projects/Projects";
 import { Footer } from "./components/Footer/Footer";
 gsap.registerPlugin(useGSAP);
+
+const Background = lazy(() => import("./components/Background/Background"));
 
 const App: React.FC = () => {
   const [loading, setLoading] = useState(true);
@@ -16,7 +17,7 @@ const App: React.FC = () => {
     <>
       {loading && <Preloader onComplete={() => setLoading(false)} />}
       <Navbar />
-      {/* <Background /> */}
+      <Background />
       <main role="main" style={{ opacity: loading ? 0 : 1, transition: "opacity 1.5s ease", }}>
         <Hero />
         <Projects />
