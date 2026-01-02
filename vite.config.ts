0192a5e9eb239d61af31ec55c57fb9bbc627d185
Samuel Rivera <seriveramosq@gmail.com>
2026-01-02 12:29:21 -0500

    fix: main route

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,7 @@ import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [sveltekit(), devtoolsJson()] });
+export default defineConfig({
+    plugins: [sveltekit(), devtoolsJson()],
+    base: process.env.BASE_PATH ? '/' + process.env.BASE_PATH + '/' : '/'
+});
