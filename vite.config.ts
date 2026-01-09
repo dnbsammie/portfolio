87e9e72048936fd5611c9e78b859acf18189943f
Samuel Rivera <seriveramosq@gmail.com>
2026-01-08 22:10:01 -0500

    fix: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
-    plugins: [sveltekit(), devtoolsJson(),glsl()]
+    plugins: [sveltekit(), devtoolsJson()]
 });
